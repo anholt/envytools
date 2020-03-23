2c36ea9336ce33d4dc0ed5cd35db5d7dd2fbc0d0
Eric Anholt <eric@anholt.net>
2020-03-23 10:45:47 -0700

    add ubwc to script

--- cffdump/scripts/tex3d-layout.lua
+++ cffdump/scripts/tex3d-layout.lua
@@ -53,6 +53,9 @@ function draw(primtype, nindx)
   blit.pitch   = r.RB_2D_DST_SIZE.PITCH
   blit.addr    = r.RB_2D_DST_LO | (r.RB_2D_DST_HI << 32)
   blit.base    = bos.base(blit.addr)
+  blit.ubwc_addr = r.RB_2D_DST_FLAGS_LO | (r.RB_2D_DST_FLAGS_HI << 32)
+  blit.ubwc_base = bos.base(blit.uwbc_addr)
+  blit.ubwc_pitch = r.RB_2D_DST_FLAGS_PITCH.PITCH
   blit.endaddr = 0  -- filled in later
   printf("Found blit: 0x%x (0x%x)\n", blit.addr, blit.base)
 
@@ -65,6 +68,7 @@ function A6XX_TEX_CONST(pkt, size)
   -- are looking for
 
   local base = pkt[4].BASE_LO | (pkt[5].BASE_HI << 32)
+  local ubwc_base = pkt[7].FLAG_LO | (pkt[8].FLAG_HI << 32)
   local width0  = pkt[1].WIDTH
   local height0 = pkt[1].HEIGHT
   local depth0  = pkt[5].DEPTH
@@ -74,13 +78,14 @@ function A6XX_TEX_CONST(pkt, size)
   end
   found_tex = 1
 
-  printf("Found texture state: %ux%ux%u (%s, MIN_LAYERSZ=0x%x)\n",
-         width0, height0, depth0, pkt[0].TILE_MODE, pkt[3].MIN_LAYERSZ)
+  printf("Found texture state:\n  %ux%ux%u (%s, %s, MIN_LAYERSZ=0x%x, TILE_ALL=%s, UBWC=%s)\n",
+         width0, height0, depth0, pkt[0].FMT, pkt[0].TILE_MODE, pkt[3].MIN_LAYERSZ, tostring(pkt[3].TILE_ALL), tostring(pkt[3].FLAG))
 
   -- Note that in some case the texture has some extra page or so
   -- at the beginning:
   local basebase = bos.base(base)
   printf("base: 0x%x (0x%x)\n", base, basebase)
+  printf("ubwcbase: 0x%x (0x%x)\n", ubwc_base, bos.base(ubwc_base))
 
   -- see if we can find the associated blits..  The blob always seems to
   -- start from the lower (larger) mipmap levels and layers, so we don't
@@ -107,6 +112,10 @@ function A6XX_TEX_CONST(pkt, size)
   local layer = 0
   local w = width0   -- track current width/height to detect changing
   local h = height0  -- mipmap level
+
+  printf("		.layout = {\n")
+  printf("			.width0 = %d, .height0 = %d,\n", width0, height0)
+  printf("			.slices = {\n")
   for n = 0,nblits-1 do
     local blit = blits[n]
     --printf("%u: %ux%u, addr=%x\n", n, blit.width, blit.height, blit.addr)
@@ -125,17 +134,47 @@ function A6XX_TEX_CONST(pkt, size)
       h = blit.height
     end
 
-    printf("level=%u, layer=%u, sz=%ux%u, pitch=%u, offset=0x%x, addr=%x",
-           level, layer, w, h, blit.pitch, blit.addr - base, blit.addr)
-    if blit.endaddr ~= 0 then
-      local layersz = blit.endaddr - blit.addr
-      local alignedheight = layersz / blit.pitch
-      printf(", layersz=0x%x, alignedheight=%f", layersz, alignedheight)
-    end
-    printf("\n")
+    printf("				{ .offset = %d, .pitch = %u },\n",
+        blit.addr - base,
+    	blit.pitch
+    );
 
     layer = layer + 1
   end
+
+  layer = 0
+  level = 0
+  w = width0
+  h = height0
+
+  printf("			.ubwc_slices = {\n")
+  for n = 0,nblits-1 do
+    local blit = blits[n]
+    --printf("%u: %ux%u, addr=%x\n", n, blit.width, blit.height, blit.addr)
+    if w ~= blit.width or h ~= blit.height then
+      level = level + 1
+      layer = 0
+
+      if blit.width ~= minify(w, 1) or blit.height ~= minify(h, 1) then
+        printf("I am confused! %ux%u vs %ux%u\n", blit.width, blit.height, minify(w, 1), minify(h, 1))
+	printf("addr=%x\n", blit.addr)
+        --return
+      end
+
+      w = blit.width
+      h = blit.height
+    end
+
+    printf("				{ .offset = %d, .pitch = %u },\n",
+        blit.ubwc_addr - ubwc_base,
+    	blit.ubwc_pitch
+    );
+
+    layer = layer + 1
+  end
+
+  printf("			},\n")
+  printf("		},\n")
   printf("\n\n")
 end
 
